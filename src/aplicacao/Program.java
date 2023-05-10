28c70172b84ea7cded9aed3c9c959ce3431fe1f4
leopimentl <leandrokhalelpimentel@gmail.com>
2023-05-10 17:53:47 -0300

    Primeira classe

---
+++ src/aplicacao/Program.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Posicao pos = new Posicao(3, 5);
+        System.out.println(pos);
+    }
+}
